635c8bb01bbf16a1e49d0b333bea070bd160cb82
leviwfrey <levster2004@yahoo.com>
2023-11-28 13:12:43 -0800

    draws a square

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <GL/glut.h>
 
-int main() {
-    
+
+void displayMe(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.5, 0.5, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+    glEnd();
+    glFlush();
+}
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(1920, 1080);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("Hello");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
 }
