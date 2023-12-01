467030102346abac335f41c5facd1b9fc6172893
ucsbjonas <jonas_chen@ucsb.edu>
2023-11-30 21:04:42 -0800

    some reorg + random npc squares

--- src/lib/Entity.h
+++ src/lib/Entity.h
@@ -1,11 +1,59 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 #include "Point.h"
+#include "Draw.h"
 
 class Entity { 
     public:
         virtual void draw() = 0;
-        virtual void update() = 0;
+};
+
+class Player: public Entity {
+
+    public:
+        Player(double _size, double _max_vel): max_vel(_max_vel) {
+            p = {0,0}; //sets the initial point
+            size = _size;
+        }
+        void draw() override{
+            drawSquare(size, p);
+        }
+        void update(double controllerX, double controllerY) {
+            x_vel = max_vel*controllerX;
+            y_vel = max_vel*controllerY;
+            p.x += x_vel;
+            p.y += y_vel;
+        }
+
+        Point p;
+        double size;
+        const double max_vel;
+        double x_vel = 0; 
+        double y_vel = 0;    
+};
+
+class Npc: public Entity {
+
+    public:
+        Npc(double _size, double _max_vel, double initialX, double initialY): max_vel(_max_vel) {
+            p = {initialX, initialY}; //sets the initial point
+            size = _size;
+        }
+        void draw() override{
+            drawSquare(size, p);
+        }
+        void update(double deltaX, double deltaY) {
+            x_vel = max_vel*deltaX;
+            y_vel = max_vel*deltaY;
+            p.x += x_vel;
+            p.y += y_vel;
+        }
+
+        Point p;
+        double size;
+        const double max_vel;
+        double x_vel = 0;
+        double y_vel = 0;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,55 @@
 #include <iostream>
 #include <GL/glut.h>
-#include <Draw.h>
+#include <random>
+#include <vector>
+
+#include "Draw.h"
+#include "Entity.h"
 #include "Controller.h"
 
-// Makes a square move accross the screen forever
+//Global Constants
+size_t const WINDOW_DIM_1 = 1400;
+size_t const WINDOW_DIM_2 = 1400;
+
+
+// Instansiate needed classes
 Controller controller;
-Point p = {0, 0};
-
-// you can change these to make it faster or bigger
-double size = .06;
-const double max_vel = .015;
-
-double x_vel = 0;
-double y_vel = 0;
+Player player(0.12, 0.015); //see constructor for instantitaion details
+std::vector<Npc> npc_vec;
 
 
-
+//Random Number generation
+std::random_device rd;
+std::uniform_real_distribution<double> distribution(-1, 1);
+auto gen = std::default_random_engine(rd()); //distribution(gen) will generate a random number
 
 void update(int value) {
     static_cast<void>(value);
     controller.update();
-    x_vel = max_vel*controller.getX();
-    y_vel = max_vel*controller.getY();
-    p.x += x_vel;
-    p.y += y_vel;
+    player.update(controller.getX(), controller.getY());
+    for(size_t i = 0; i < npc_vec.size(); ++i){ //updates the npc position
+        npc_vec.at(i).update(distribution(gen), distribution(gen));
+    }
     
-    glutPostRedisplay(); // calls the display function
+    glutPostRedisplay();          // calls the display function
     glutTimerFunc(16, update, 0); // calls the update function again after 16 milliseconds (60FPS)
 }
 
+void spawnNpcs(size_t num_to_generate) {
+    while(num_to_generate != 0){
+        Npc npc = Npc(0.06, 0.015, distribution(gen), distribution(gen)); 
+        npc.draw();
+        npc_vec.push_back(npc);
+        num_to_generate -= 1;
+    }
+}
+
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT); // clears the canvas
-    drawSquare(size, p); // draws a square on the canvas at the point
+    glClear(GL_COLOR_BUFFER_BIT);                // clears the canvas
+    player.draw();                               // draws a square on the canvas at the point
+    for(size_t i = 0; i < npc_vec.size(); ++i){  //draws the npcs
+        npc_vec.at(i).draw();
+    }
     glutSwapBuffers();  // swaps the canvas with the current screen
 }
 
@@ -50,13 +68,14 @@ void handleKeyReleased(unsigned char key, int x, int y) {
 int main(int argc, char** argv) {
 
     // Initiation:
-    glutInit(&argc, argv); // Intiates screen?
-    glutInitDisplayMode(GLUT_SINGLE); //You need to do once ig
-    glutInitWindowSize(1400, 1400); //sets the size, might be pixel by pixel
-    glutInitWindowPosition(0,0);    //position of window on screen
+    glutInit(&argc, argv);                          // Intiates screen?
+    glutInitDisplayMode(GLUT_SINGLE);               //You need to do once ig
+    glutInitWindowSize(WINDOW_DIM_1, WINDOW_DIM_2); //sets the size, might be pixel by pixel
+    glutInitWindowPosition(0,0);                    //position of window on screen
     glutCreateWindow("Funny little square!!"); 
 
     // Game Loop:
+    spawnNpcs(5);
     glutDisplayFunc(display); // function that paints everything on the screen
     glutTimerFunc(25, update, 0); // function that should do all the logic for entities
     glutKeyboardFunc(handleKeyPress);
